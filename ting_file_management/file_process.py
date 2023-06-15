7d6207b1f624ff23ebc53ca79cfdc7e8100eb144
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-06-14 22:43:53 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+# import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": (file),
+    }
+    instance.enqueue(content)
+    print(content)
+    return content
 
 
 def remove(instance):
