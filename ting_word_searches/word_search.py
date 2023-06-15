ce8e17fb7eb8394b968cb63d11b9ba9f814d3431
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-06-15 18:30:01 -0300

    req 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -15,8 +15,28 @@ def exists_word(word, instance):
                     "ocorrencias": lines,
                 }
             )
+
     return ocorre
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    ocorre = []
+    lines = []
+    for index in range(len(instance)):
+        file_info = instance.search(index)
+        for instance, line in enumerate(file_info["linhas_do_arquivo"]):
+            if word.casefold() in line.casefold():
+                lines.append(
+                    {"linha": instance + 1, "conteudo": line}
+                )
+        if len(lines) > 0:
+            ocorre.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_info["nome_do_arquivo"],
+                    "ocorrencias": lines,
+                }
+            )
+
+    return ocorre
